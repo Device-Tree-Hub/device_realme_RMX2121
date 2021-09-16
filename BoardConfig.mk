b56fce84713e7454abaa76947eb0736c88c060d2
Sarthak Roy <sarthakroy2002@gmail.com>
2021-09-16 09:21:35 +0530

    RMX2121: Define device architecture
    Change-Id: Ia7f175e9abadb74d1edd7498aeddbb8cf8215bdd
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,3 +15,16 @@
 #
 
 DEVICE_PATH := device/realme/RMX2121
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
