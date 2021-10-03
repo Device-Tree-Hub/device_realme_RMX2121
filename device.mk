2fbb571e945ab61e1531dc5a188f7b611b1dc067
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2021-10-03 13:03:50 +0000

    RMX2121: Enable refresh_rate_controls Setting
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-tenx
 
 # Permissions
 PRODUCT_COPY_FILES += \
