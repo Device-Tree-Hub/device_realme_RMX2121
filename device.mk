91f08c0cbf5a99b69875bff5508a5325488ea5ef
Advaith Bhat <advaithbhat9@gmail.com>
2021-10-13 09:50:48 +0000

    RMX2121: Build Health HAL from source
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
