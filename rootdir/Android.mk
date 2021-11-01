fcdacbc2b2b0e24d3d45e504d0af666e92eebcd8
Advaith Bhat <advaithbhat9@gmail.com>
2021-11-01 04:33:16 +0000

    RMX2121: Fix init derp
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,10 +25,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mt6768.rc
+LOCAL_MODULE       := init.mt6889.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mt6768.rc
+LOCAL_SRC_FILES    := etc/init.mt6889.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
