f8b67e88064807666411e8353e683185394ad6ca
PeterCxy <peter@typeblog.net>
2021-11-02 12:56:37 +0000

    RMX2121: Implement FOD HAL & adapt to Realme X7 Pro
    Co-authored-by: Harshit Jain <god@harsh1998.me>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- device.mk
+++ device.mk
@@ -55,7 +55,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.rmx2121
+    android.hardware.biometrics.fingerprint@2.1-service.rmx2121 \
+    lineage.biometrics.fingerprint.inscreen@1.0-service.RMX2121
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
