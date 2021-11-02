2aa6e615944d9c28fb8a5dffda76388ade36a397
Advaith Bhat <advaithbhat9@gmail.com>
2021-11-02 12:59:14 +0000

    RMX2121: Build Oppo Fingerprint HAL
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- device.mk
+++ device.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2021 The LineageOS Project
 #
@@ -56,7 +57,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.rmx2121 \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.RMX2121
+    lineage.biometrics.fingerprint.inscreen@1.0-service.RMX2121 \
+    vendor.oppo.hardware.biometrics.fingerprint@2.1
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
