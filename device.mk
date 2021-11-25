7663c58a403b4c70767698e120f509a1a6fc6cb0
Advaith Bhat <advaithbhat9@gmail.com>
2021-11-25 07:13:19 +0000

    RMX2121: Remove Doze
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- device.mk
+++ device.mk
@@ -54,10 +54,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
-# Doze
-PRODUCT_PACKAGES += \
-    RealmeDoze
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.rmx2121 \
